6367b5606395905df044b67f256b149250d04761
joelalive <joelimalive1994@gmail.com>
2019-08-28 21:06:53 +0530

    yield and section

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"  crossorigin="anonymous">
-    <title>Todos</title>
-</head>
-<body>
+@extends('layouts.app')
 
-<div class="container">
+
+@section('title')
+Todos
+@endsection
+
+@section('content')
+
 <h1 class="text-center my-5">Todos Page</h1>
 <div class="row justify-content-center">
     <div class="col-md-8">
@@ -32,9 +29,5 @@
         </div>
     </div>
 </div>
-</div>
-    
 
-
-</body>
-</html>
+@endsection
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,19 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"  crossorigin="anonymous">
-    <title>Todo Item</title>
-</head>
-<body>
+@extends('layouts.app')
 
-<div class="container">
-    <h1 class="text-center my-5">
+
+@section('title')
+Single Todo: {{$todo->name}}
+@endsection
+
+@section('content')
+
+<h1 class="text-center my-5">
         {{$todo->name}}
-    </h1>
-</div>
+</h1>
+
 
 <div class="row justify-content-center">
     <div class="col-md-6">
@@ -28,6 +25,4 @@
     </div>
 </div>
 
-
-</body>
-</html>
+@endsection
